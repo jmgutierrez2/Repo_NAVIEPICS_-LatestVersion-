4e54c634fda72b091554e65946070376c9383012
JM Gutiérrez <jmguticasani1254@gmail.com>
2024-05-11 18:19:30 -0500

    Agregando proyecto

---
+++ src/main/java/com/naviepics/NAVIEPICS/NaviepicsApplication.java
@@ -0,0 +1,13 @@
+package com.naviepics.NAVIEPICS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaviepicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaviepicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naviepics/NAVIEPICS/NaviepicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naviepics.NAVIEPICS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaviepicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
