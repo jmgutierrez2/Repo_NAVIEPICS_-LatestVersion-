16e0a790d21da2d39346f5ff0866b0ba1529bf3e
Rodrigo-Barrios <rodrigo_barrios@usmp.pe>
2024-05-28 01:34:01 -0500

    Update Espacios.java

--- src/main/java/com/naviepics/model/MySQL/Espacios.java
+++ src/main/java/com/naviepics/model/MySQL/Espacios.java
@@ -1,5 +1,101 @@
 package com.naviepics.model.MySQL;
 
-public class Espacios {
+import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name ="Espacios")
+public class Espacios {
+	
+	@Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="Id_Espacio")
+    private long id;
+	
+	@Column(name="Estado", nullable=false)
+    private Integer estado;
+	
+	@ManyToOne
+	@JoinColumn(name="Id_Estacionamiento",  nullable=false)
+	private Estacionamiento estacionamiento;
+	
+	@ManyToOne
+	@JoinColumn(name="Id_Tipo_Espacios",  nullable=false)
+	private Tipo_Espacios tipo_espacios;
+	
+	@OneToMany(mappedBy="espacios")
+	private List<Recibo> listaRecibo;
+	
+	@OneToMany(mappedBy="espacios")
+	private List<Reservaciones> listaReservaciones;
+
+	public Espacios(long id, Integer estado, Estacionamiento estacionamiento, Tipo_Espacios tipo_espacios,
+			List<Recibo> listaRecibo, List<Reservaciones> listaReservaciones) {
+		super();
+		this.id = id;
+		this.estado = estado;
+		this.estacionamiento = estacionamiento;
+		this.tipo_espacios = tipo_espacios;
+		this.listaRecibo = listaRecibo;
+		this.listaReservaciones = listaReservaciones;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Integer getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Integer estado) {
+		this.estado = estado;
+	}
+
+	public Estacionamiento getEstacionamiento() {
+		return estacionamiento;
+	}
+
+	public void setEstacionamiento(Estacionamiento estacionamiento) {
+		this.estacionamiento = estacionamiento;
+	}
+
+	public Tipo_Espacios getTipo_espacios() {
+		return tipo_espacios;
+	}
+
+	public void setTipo_espacios(Tipo_Espacios tipo_espacios) {
+		this.tipo_espacios = tipo_espacios;
+	}
+
+	public List<Recibo> getListaRecibo() {
+		return listaRecibo;
+	}
+
+	public void setListaRecibo(List<Recibo> listaRecibo) {
+		this.listaRecibo = listaRecibo;
+	}
+
+	public List<Reservaciones> getListaReservaciones() {
+		return listaReservaciones;
+	}
+
+	public void setListaReservaciones(List<Reservaciones> listaReservaciones) {
+		this.listaReservaciones = listaReservaciones;
+	}
+	
+	
 }
