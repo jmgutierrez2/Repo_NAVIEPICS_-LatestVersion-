fe4aafdbbc35f554e5e36e091cbe002305e5a705
Rodrigo-Barrios <rodrigo_barrios@usmp.pe>
2024-05-28 01:34:42 -0500

    Update Tipo_Vehiculo.java

--- src/main/java/com/naviepics/model/MySQL/Tipo_Vehiculo.java
+++ src/main/java/com/naviepics/model/MySQL/Tipo_Vehiculo.java
@@ -1,5 +1,61 @@
 package com.naviepics.model.MySQL;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name ="Tipo_Vehiculo")
 public class Tipo_Vehiculo {
+	
+	@Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="Id_Tipo_Vehiculo")
+    private long id;
+	
+	@Column(name="Descripcion", nullable=false)
+	private String descripcion;
+	
+	@OneToMany(mappedBy="tipo_vehiculo")
+	private List<Vehiculo> listaVehiculo;
+
+	public Tipo_Vehiculo(long id, String descripcion, List<Vehiculo> listaVehiculo) {
+		super();
+		this.id = id;
+		this.descripcion = descripcion;
+		this.listaVehiculo = listaVehiculo;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public List<Vehiculo> getListaVehiculo() {
+		return listaVehiculo;
+	}
+
+	public void setListaVehiculo(List<Vehiculo> listaVehiculo) {
+		this.listaVehiculo = listaVehiculo;
+	}
+	
+	
 
 }
