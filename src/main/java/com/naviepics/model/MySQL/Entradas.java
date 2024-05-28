b1178b3936988adce8c9f9a1461c3fcac6d4f7a2
Rodrigo-Barrios <rodrigo_barrios@usmp.pe>
2024-05-28 00:32:59 -0500

    Update Entradas.java

--- src/main/java/com/naviepics/model/MySQL/Entradas.java
+++ src/main/java/com/naviepics/model/MySQL/Entradas.java
@@ -3,7 +3,9 @@ package com.naviepics.model.MySQL;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+/*{{{
+ 
+ */
 @Entity
 @Table(name="Entradas")
 public class Entradas {
