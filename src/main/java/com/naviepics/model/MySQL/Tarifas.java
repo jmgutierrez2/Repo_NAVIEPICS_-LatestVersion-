b0e256e00048a554137a9d94525df479062b9f02
Rodrigo-Barrios <rodrigo_barrios@usmp.pe>
2024-05-31 01:29:55 -0500

    Update Tarifas.java

--- src/main/java/com/naviepics/model/MySQL/Tarifas.java
+++ src/main/java/com/naviepics/model/MySQL/Tarifas.java
@@ -7,9 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name="Tarifas")
