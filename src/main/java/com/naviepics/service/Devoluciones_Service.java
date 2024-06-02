dde2f453db4dd3f6987b402be2cb606c75661e35
JM Gutiérrez <jmguticasani1254@gmail.com>
2024-06-01 20:29:24 -0500

    A

--- src/main/java/com/naviepics/service/Devoluciones_Service.java
+++ src/main/java/com/naviepics/service/Devoluciones_Service.java
@@ -1,11 +1,38 @@
 package com.naviepics.service;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.naviepics.model.MySQL.Devoluciones;
+import com.naviepics.repo.MySQL.Devoluciones_Repo;
 
-public interface Devoluciones_Service {
+public class Devoluciones_Service {
 
-	public List<Devoluciones> listarDevoluciones();
+	@Autowired
+	private Devoluciones_Repo repo;
+	
+	@Transactional
+	public void registrar(Devoluciones t) {
+		repo.save(t);
+	}
+	@Transactional
+	public Optional<Devoluciones> mostrarUno(long id) {
+		return repo.findById(id);
+	}
+	@Transactional
+	public List<Devoluciones> mostrarTodo(){
+		return repo.findAll();
+	}
+	@Transactional
+	public Devoluciones actualizar(Devoluciones d) {
+		return repo.save(d);
+	}
+	@Transactional
+	public void eliminar(long id) {
+		repo.deleteById(id);
+	}
 	
 }
