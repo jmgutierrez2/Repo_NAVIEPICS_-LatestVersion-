b237241a2fe864b0d97b52c2c754434ef0357675
JM Gutiérrez <jmguticasani1254@gmail.com>
2024-06-02 18:15:17 -0500

    t

--- src/main/java/com/naviepics/service/Devoluciones_Service.java
+++ src/main/java/com/naviepics/service/Devoluciones_Service.java
@@ -3,12 +3,7 @@ package com.naviepics.service;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.naviepics.model.MySQL.Devoluciones;
-import com.naviepics.repo.MySQL.Devoluciones_Repo;
 
 public interface Devoluciones_Service {
 	
